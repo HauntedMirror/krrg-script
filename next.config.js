2aa8ee9fcd494b286a3b6c41569140cfb9544085
HauntedMirror <claricatcutter@gmail.com>
2023-04-24 18:45:09 +0900

    fix next-config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const repo = 'change-me-to-your-repo';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true,
   trailingSlash: true,
 }

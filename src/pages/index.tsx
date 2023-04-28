d9041d889d257e1386ba5b15c1040e0faf1c23d9
HauntedMirror <claricatcutter@gmail.com>
2023-04-29 00:44:29 +0900

    ヘッダの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,16 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import ProgramArea from "./components/ProgramArea";
 import Description from "./components/Description";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <main>
-      <Box display="flex" justifyContent="center" p={1}>
-        <Typography variant="h3">くるるぎすくりぷと</Typography>
-      </Box>
+      <Header />
       <Grid container justifyContent="center" alignItems="flex-start">
         <Grid item xs>
           <ProgramArea />
+          <pre></pre>
         </Grid>
         <Grid item xs>
           <Description />
